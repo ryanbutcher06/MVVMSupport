55f3b24d4e010ec389ca7e3187c36dcc8ec03891
Ryan Butcher <ryanbutcher06@gmail.com>
2013-10-05 18:20:50 -0400

    Fixed the missing construtor for the PropertyChangedMesage.

--- src/MVVMSupport/MVVMSupport/Messenger/PropertyChangedMessage.cs
+++ src/MVVMSupport/MVVMSupport/Messenger/PropertyChangedMessage.cs
@@ -7,6 +7,19 @@
     public class PropertyChangedMessage<TSender> : IMessage
     {
         /// <summary>
+        /// Initilizes a new instance of PropertyChnagedMessage.
+        /// </summary>
+        /// <param name="name">The name of the message.</param>
+        /// <param name="propertyName">The name of the property associated with the message.</param>
+        /// <param name="sender">The sender of the message.</param>
+        public PropertyChangedMessage(string name, string propertyName, TSender sender)
+        {
+            Name = name;
+            PropertyName = propertyName;
+            Sender = sender;
+        }
+
+        /// <summary>
         /// Gets the name of the message.
         /// </summary>
         public string Name { get; private set; }
